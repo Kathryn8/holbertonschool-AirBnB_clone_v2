0c781acda4a7f7448a605e3c591c8e0085c0cde0
Kathryn6368 <6368@holbertonstudents.com>
2023-08-02 04:43:25 -0700

    Add a new public instance method delete(self, obj=None) and update all(self) to all(self, cls=None)

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
@@ -8,9 +15,23 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    classes = {
+        'BaseModel': BaseModel, 'User': User, 'Place': Place,
+        'State': State, 'City': City, 'Amenity': Amenity,
+        'Review': Review
+    }
+
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        og_dict = FileStorage.__objects
+        if cls is None:
+            return og_dict
+        class_name = iter(
+            {i for i in FileStorage.classes if FileStorage.classes[i] == cls}
+        )
+        x = next(class_name)
+        filtered_dict = {k: v for k, v in og_dict.items() if k.startswith(x)}
+        return filtered_dict
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,6 +45,13 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        """ to delete obj from __objects if it’s inside"""
+        if obj is None:
+            pass
+        obj_key = (obj.to_dict()['__class__'] + '.' + obj.id)
+        FileStorage.__objects.pop(obj_key)
 
     def reload(self):
         """Loads storage dictionary from file"""
@@ -45,6 +73,6 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
