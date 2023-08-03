e32dd1fb36300771c613df35286bdb4c832ca621
Kathryn6368 <6368@holbertonstudents.com>
2023-08-02 22:08:50 -0700

    update for task 6

--- models/state.py
+++ models/state.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, Integer, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from models import storage
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """Class definition for the state class """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship(
+        "City", back-populates="state", cascade="all, delete, delete-orphan")
+
+    @property
+    def cities(self):
+        """returns the list of City instance that belong to that State"""
+        all_cities_dict = storage.all(City)
+        return_list = []
+        for key, value in all_cities_dict.items():
+            if self.id == value.state_id:
+                return_list.append(value)
+        return return_list
