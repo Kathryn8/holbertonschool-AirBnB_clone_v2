3928dfa1bbf16077f4398e961c108fab802e76d2
BryanField15 <6367@holbertonstudents.com>
2023-08-03 15:46:34 -0700

    updated reload

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -5,7 +5,7 @@ but it will be interacting with a SQL database instead of JSON files.
 import os
 from sqlalchemy import create_engine
 from models.base_model import Base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 class DBStorage:
@@ -56,4 +56,17 @@ class DBStorage:
             self.__session.delete(obj)
 
     def reload(self):
-        """
+        """create all tables in the database"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        Base.metadata.create_all(self.__engine)
+
+        Session = scoped_session(sessionmaker(bind=self.__engine,
+                                              expire_on_commit=False))
+        self.__session = Session()
