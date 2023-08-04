2a9d0b4e8f7f183546a8e4f82575f08d750353c4
BryanField15 <6367@holbertonstudents.com>
2023-08-04 02:43:44 -0700

    updated

--- console.py
+++ console.py
@@ -131,7 +131,7 @@ class HBNBCommand(cmd.Cmd):
         if len(kwargs) != 0:
             for key, value in kwargs.items():
                 setattr(new_instance, key, value)
-        storage.save()
+        new_instance.save()
         print(new_instance.id)
         storage.save()
 
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,8 +4,14 @@ but it will be interacting with a SQL database instead of JSON files.
 """
 import os
 from sqlalchemy import create_engine
-from models.base_model import Base
+from models.base_model import BaseModel, Base
 from sqlalchemy.orm import scoped_session, sessionmaker
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class DBStorage:
@@ -31,15 +37,23 @@ class DBStorage:
     def all(self, cls=None):
         """query on the current database session"""
         if cls is None:
+            print("Querying all objects")
             object_list = self.__session.query(
             User, State, City, Amenity, Place, Review).all()
         else:
+            print(f"Querying objects of class {cls}")
             object_list = self.__session.query(cls).all()
+
+        print(f"Retrieved {len(object_list)} objects from the database")
+
         new_dict = {}
         for obj in object_list:
             obj_key = (obj.to_dict()['__class__'] + '.' + obj.id)
             # can we use to_dict on these obj?
             new_dict[obj_key] = obj
+
+        print(f"Returning a dictionary with {len(new_dict)} entries")
+
         return new_dict
 
     def new(self, obj):
@@ -57,16 +71,10 @@ class DBStorage:
 
     def reload(self):
         """create all tables in the database"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
 
         Base.metadata.create_all(self.__engine)
 
-        Session = scoped_session(sessionmaker(bind=self.__engine,
+        self.__session = scoped_session(sessionmaker(bind=self.__engine,
                                               expire_on_commit=False))
-        self.__session = Session()
+
+        #self.__session = Session()
