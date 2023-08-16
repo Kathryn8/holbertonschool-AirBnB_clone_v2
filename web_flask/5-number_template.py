dcc13597632b3e8e10e11f0063f97ad62ff2ea12
Kathryn6368 <6368@holbertonstudents.com>
2023-08-16 00:32:38 -0700

    add file

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application"""
+from flask import Flask, render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def helloHBNB():
+    """Displays a string to stdout"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hBNB():
+    """Displays a string to stdout"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cText(text):
+    """Displays a string to stdout"""
+    text = text.replace('_', ' ')
+    return f'C {escape(text)}'
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythonText(text="is cool"):
+    """Displays a string to stdout"""
+    text = text.replace('_', ' ')
+    return f'Python {escape(text)}'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """Displays a string to stdout"""
+    return f'{escape(n)} is a number'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """Displays a string to stdout"""
+    return render_template('5-number.html', number=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
