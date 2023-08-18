a7e18c54a613c49285b772eb2daed5d3b6ab28c9
Kathryn6368 <6368@holbertonstudents.com>
2023-08-17 18:31:44 -0700

    add if/else logic to handle db ENV variable

--- models/state.py
+++ models/state.py
@@ -10,17 +10,21 @@ class State(BaseModel, Base):
     """Class definition for the state class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship(
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """returns the list of City instance that belong to that State"""
+            from models import storage
+            from models.city import City
+            all_cities_dict = storage.all(City)
+            return_list = []
+            for key, value in all_cities_dict.items():
+                if self.id == value.state_id:
+                    return_list.append(value)
+            return return_list
+    else:
+        cities = relationship(
         "City", back_populates="state", cascade="all, delete, delete-orphan")
 
-    @property
-    def cities(self):
-        """returns the list of City instance that belong to that State"""
-        from models import storage
-        from models.city import City
-        all_cities_dict = storage.all(City)
-        return_list = []
-        for key, value in all_cities_dict.items():
-            if self.id == value.state_id:
-                return_list.append(value)
-        return return_list
+        
