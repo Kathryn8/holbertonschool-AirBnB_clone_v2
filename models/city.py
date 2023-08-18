08c7937edb26edeadf3e7841ece15c9a0d8e844f
Kathryn6368 <6368@holbertonstudents.com>
2023-08-17 19:30:40 -0700

    include db switch logic

--- models/city.py
+++ models/city.py
@@ -7,12 +7,14 @@ from sqlalchemy import Column, Integer, DateTime, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import ForeignKey
+from os import getenv
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    places = relationship(
-        "Place", back_populates="cities", cascade="all, delete, delete-orphan")
-    state = relationship("State", back_populates="cities")
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        places = relationship(
+            "Place", back_populates="cities", cascade="all, delete, delete-orphan")
+        state = relationship("State", back_populates="cities")
