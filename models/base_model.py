e78038e1b4998a70b402e42f6559636b032eacb9
Kathryn6368 <6368@holbertonstudents.com>
2023-08-17 17:52:21 -0700

    change the instantiation method to better handle kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -15,25 +15,32 @@ class BaseModel:
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """Instantiates a new model"""
+        attributes_ignore_list = ["__class__"]
+        attribute_datetime_list = ['created_at', 'updated_at']
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+
         if not kwargs:
-            #from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-        else:
-            if 'updated_at' in kwargs:
-                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                         '%Y-%m-%dT%H:%M:%S.%f')
-            if 'created_at' in kwargs:
-                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                         '%Y-%m-%dT%H:%M:%S.%f')
-            if '__class__' in kwargs:
-                del kwargs['__class__']
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-            self.__dict__.update(kwargs)
 
+        else:   # we have kwargs passed in
+            for key, value in kwargs.items():
+                if key in attributes_ignore_list:
+                    continue
+                elif key in attribute_datetime_list:
+                    v = datetime.strptime(value, time_format)
+                    setattr(self, key, v)
+                else:
+                    setattr(self, key, value)
+
+            if 'id' not in kwargs:
+                setattr(self, 'id', str(uuid.uuid4()))
+            if 'created_at' not in kwargs:
+                setattr(self, 'created_at', datetime.now())
+            if 'update_at' not in kwargs:
+                setattr(self, 'updated_at', datetime.now())
 
     def __str__(self):
         """Returns a string representation of the instance"""
