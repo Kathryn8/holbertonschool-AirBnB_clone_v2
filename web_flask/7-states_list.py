8411161bac9e1b2b8b228f384f17861f8e85be73
Kathryn6368 <6368@holbertonstudents.com>
2023-08-17 23:36:44 -0700

    not passing locally yet, before commenting out all refs to place

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application"""
+from flask import Flask, render_template
+from markupsafe import escape
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def list_objects():
+    """Displays a HTML page"""
+    return render_template('7-states_list.html', state=storage.all(State))
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """ remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
