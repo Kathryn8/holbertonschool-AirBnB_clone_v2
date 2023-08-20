5cfaec2f919ec4c008baf147b8bf4813b1f0344e
Kathryn6368 <6368@holbertonstudents.com>
2023-08-19 19:54:24 -0700

    comment our amenity connections

--- models/place.py
+++ models/place.py
@@ -9,6 +9,7 @@ from models.amenity import Amenity
 from models.review import Review
 from os import getenv
 
+"""
 place_amenity = Table(
     "place_amenity", Base.metadata,
     Column("place_id",
@@ -22,12 +23,13 @@ place_amenity = Table(
            primary_key=True,
            nullable=False)
 )
+"""
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
-    #city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
@@ -37,15 +39,15 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer(), default=0, nullable=False)
     latitude = Column(Float(), nullable=True)
     longitude = Column(Float(), nullable=True)
-    amenity_ids = []
+    #amenity_ids = []
     if getenv("HBNB_TYPE_STORAGE") == "db":
         user = relationship("User", back_populates="places")
-        #city = relationship("City", back_populates="places")
+        city = relationship("City", back_populates="places")
         reviews = relationship(
             "Review", back_populates="place",
             cascade="all, delete, delete-orphan")
-        amenities = relationship(
-            "Amenity", secondary="place_amenity", viewonly=False)
+        #amenities = relationship(
+            #"Amenity", secondary="place_amenity", viewonly=False)
 
     @property
     def reviews(self):
@@ -57,9 +59,10 @@ class Place(BaseModel, Base):
                 review_list.append(review)
         return review_list
 
+    """
     @property
     def amenities(self):
-        """returns list of Amenity instances based on attribute amenity_ids"""
+        ""returns list of Amenity instances based on attribute amenity_ids""
         amenity_obj_list = []
         from models import storage
         for amenity in storage.all(Amenity).values():
@@ -69,7 +72,8 @@ class Place(BaseModel, Base):
 
     @amenities.setter
     def amenities(self, obj):
-        """that handles append method for adding an Amenity.id"""
+        ""that handles append method for adding an Amenity.id""
         if obj is not type(Amenity):
             return
         self.amenity_ids.append(obj.id)
+    """
